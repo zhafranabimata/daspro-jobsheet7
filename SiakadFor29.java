8d01a218aa104c315ce493cb7e18fc1ba08c5be8
Zhafranabimata <adyaganteng981@gmail.com>
2025-10-25 19:18:23 +0700

    Modifikasi Kode

--- SiakadFor29.java
+++ SiakadFor29.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class SiakadFor29 {
     public static void main(String[] args) {
         double nilai, tertinggi = 0, terendah = 100;
+        int mahalulus = 0, mahatidak = 0;
+
         Scanner sc = new Scanner(System.in);
         
         for (int i = 1; i <= 10; i++) {
@@ -14,10 +16,18 @@ public class SiakadFor29 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai < 60) {
+                mahatidak++;
+            }
+            if (nilai >= 60) {
+                mahalulus++;
+            }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus: " + mahalulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus: " + mahatidak);
 
         sc.close();
     }
